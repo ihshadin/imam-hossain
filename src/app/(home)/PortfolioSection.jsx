331de6c181221301fdbb7ebfaa843ff60b38808b
ihshadin <ihshadin3453@gmail.com>
2023-12-30 01:59:38 +0600

    Portfolio section design done

--- src/app/(home)/PortfolioSection.jsx
+++ src/app/(home)/PortfolioSection.jsx
@@ -1,15 +1,80 @@
 import SectionTitle from "@/components/Titles/SectionTitle";
 import React from "react";
+import portfolioImg from "@/assets/images/res_image.jpg";
+import Image from "next/image";
 
 const PortfolioSection = () => {
+  const portItems = [
+    {
+      id: 1,
+      name: "Personal Portfolio Website",
+      category: "Ecommerce Website",
+      typeOfWork: "Full Website Create",
+    },
+    {
+      id: 2,
+      name: "Personal Portfolio Website",
+      category: "LMS Website",
+      typeOfWork: "Full Website Create",
+    },
+    {
+      id: 3,
+      name: "Personal Portfolio Website",
+      category: "LMS Website",
+      typeOfWork: "Full Website Create",
+    },
+    {
+      id: 4,
+      name: "Personal Portfolio Website",
+      category: "LMS Website",
+      typeOfWork: "Full Website Create",
+    },
+    {
+      id: 5,
+      name: "Personal Portfolio Website",
+      category: "LMS Website",
+      typeOfWork: "Full Website Create",
+    },
+    {
+      id: 6,
+      name: "Personal Portfolio Website",
+      category: "LMS Website",
+      typeOfWork: "Full Website Create",
+    },
+  ];
+
   return (
     <section className="py-12 lg:py-24">
       <div className="shadin-container">
         <div>
-          <SectionTitle title={"My Latest Projects"} subTitle={"Portfolio"} />
+          <SectionTitle
+            title={"My Latest Projects"}
+            subTitle={"Portfolio"}
+            btnText={"View All"}
+            btnLink={"/portfolio"}
+          />
         </div>
-        <div className="flex flex-col md:flex-row gap-7 md:gap-2 items-center mt-8 lg:mt-14">
-          content
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-center mt-8 lg:mt-14">
+          {portItems.map((item) => (
+            <div
+              key={item.id}
+              className="bg-[#23303f] hover:bg-1 duration-200 transition-all rounded-lg shadow-md hover:shadow-lg shadow-white hover:shadow-white"
+            >
+              <div>
+                <Image src={portfolioImg} />
+              </div>
+              <div className="px-4 pb-4 pt-1">
+                <div className="text-sm font-thin flex items-center gap-2">
+                  <span>{item.typeOfWork}</span>
+                  <span className="w-1 h-1 rounded-full bg-primary"></span>
+                  <span>{item.category}</span>
+                </div>
+                <h2 className="text-xl md:text-2xl font-extrabold">
+                  {item.name}
+                </h2>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
